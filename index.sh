2f431240b6969225f47e987258f32f6109c4ad3d
Ying Wang <yingw787.personal@gmail.com>
2019-12-24 16:02:06 -0500

    Update index.sh.

--- index.sh
+++ index.sh
@@ -1,3 +1,19 @@
 #!/bin/sh -e
+#
+# This script is intended to be hosted at https://dotfiles.yingw787.com for
+# configuring Ying's personal development setup.
+#
+# Assumptions:
+# - Using Ubuntu Desktop 19.10 64-bit build
+# - Internet access available
 
-echo "Hello World"
+# Install system updates
+sudo apt-get update
+sudo apt-get full-upgrade
+
+# Install `git` version 2.20.1
+
+# `git` clone repository https://github.com/yingw787/dotfiles
+#
+# Using HTTPS instead of SSH as SSH keys must be registered with remote host
+# before SSH authentication can be passed successfully.
