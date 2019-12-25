556c5b64ab134ecc95a2ed6251cdd91ae08412da
Ying Wang <yingw787.personal@gmail.com>
2019-12-25 14:02:35 -0500

    Update setup.sh.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 #
 # Dotfiles setup from `git@github.com:yingw787/dotfiles.git`.
 #
-# Commit ID (SHA-1): 'bb44d8fb5269422a7a6458bb99c5ee8cca6728dd'
+# Commit ID (SHA-1): '6b8b1be35ff5751a369b393a766598c5f25f6bd3'
 #
 # This script is intended to be hosted at https://dotfiles.yingw787.com for
 # configuring Ying's personal development setup.
@@ -25,6 +25,10 @@ echo "$LOG_PREFIX Installing latest stable `git` for distribution."
 add-apt-repository ppa:git-core/ppa
 apt-get install git
 
+# Checkpoint `git` install to ensure:
+# - `git` install exists.
+command -v git || echo "$LOG_PREFIX git installation failed." && exit 1
+
 # `git` clone repository https://github.com/yingw787/dotfiles at HEAD to
 # directory on local.
 #
@@ -42,7 +46,38 @@ git clone \
     --progress \
     $repository $destination
 
-# Execute Ubuntu install #
-install_script="$destination/ubuntu/setup-ubuntu.sh"
-echo "$LOG_PREFIX Executing install script '$install_script'."
-bash $install_script
+# Log metadata about:
+# - `git` version
+# - `git` filesystem
+# - Environment variable $HOME
+# - Location of `dotfiles` remote repository
+# - Location of `dotfiles` local repository
+GIT_VERSION=$(git --version)
+GIT_LOCATION=$(which git)
+
+echo "$LOG_PREFIX `git` installed."
+echo "$LOG_PREFIX `git` install version: '$GIT_VERSION'"
+echo "$LOG_PREFIX `git` install filesystem: '$GIT_LOCATION'"
+echo "$LOG_PREFIX env variable '\$HOME' is: '$HOME'"
+echo "$LOG_PREFIX Location of dotfiles remote repository is: $repository"
+echo "$LOG_PREFIX Location of dotfiles local repository is: $destination"
+
+# Manually checkpoint execution to exit or continue.
+# [Ctrl + C] to exit.
+# [Enter] to continue.
+echo "Initial setup completed. Press [Ctrl + C] to quit, [Enter] to continue."
+trap quit_or_continue SIGINT
+quit_or_continue() {
+    read -s -n 1 key
+    if [[ $key = "" ]];
+    then
+        continue
+    else
+        echo "Exiting."
+        exit 0
+}
+
+# # Execute Ubuntu install #
+# install_script="$destination/ubuntu/setup-ubuntu.sh"
+# echo "$LOG_PREFIX Executing install script '$install_script'."
+# bash $install_script
