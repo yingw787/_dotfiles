8ddb96af1e08cbd4a9a1f14ad0cba0f8fda19d9a
Ying Wang <yingw787.personal@gmail.com>
2019-12-24 19:02:09 -0500

    Update post-commit.sh.

---
+++ git_hooks/post-commit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Git hook immediately after commit.
+#
+# Update `git` hooks by running command:
+# `cp post-commit.sh ../.git/hooks/post-commit`
+#
+# This script:
+# - Updates `setup.sh` with the latest SHA-1 commit to the repository.
+#
+
+COMMIT_HEAD=$(git rev-parse HEAD)
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+sed \
+    -i "s/# Commit ID (SHA-1):.*$/# Commit ID (SHA-1): $COMMIT_HEAD/" \
+    $REPO_ROOT/setup.sh
