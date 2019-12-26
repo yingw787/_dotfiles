d06fca89a7e4a244d3616fc4f6adbe52472ff080
Ying Wang <yingw787.personal@gmail.com>
2019-12-26 16:37:05 -0500

    Update configure_hugo.sh.

--- ubuntu/configure_hugo.py
+++ ubuntu/configure_hugo.py
@@ -1,15 +1,20 @@
-#!/bin/bash
+#!/usr/bin/python3.7
 #
 # Configures the 'hugo' static website generator.
 #
-# Version: 0.62.0
-# Dependencies: 'golang' (see file './configure_golang.sh' for more details)
+# Hugo Version: 0.62.0
+#
+# Hugo Dependencies: 'golang' (see file '$(pwd)/configure_golang.sh' for more
+# details).
 
-HUGO_DOWNLOAD_FILENAME="hugo_0.62.0_Linux-64bit.tar.gz"
-HUGO_DOWNLOAD_LINK="https://github.com/gohugoio/hugo/releases/download/v0.62.0/$HUGO_DOWNLOAD_FILENAME"
+import os
 
-HUGO_DOWNLOAD_DEB="hugo_0.62.0_Linux-64bit.deb"
-HUGO_DOWNLOAD_CHECKSUM="79e4594a891bf5b04997b175d549da6f3e3f913cd19c69fef7c289e50c63867c"
+
+TARBALL_FILENAME = 'hugo_0.62.0_Linux-64bit.tar.gz'
+TARBALL_URI = os.path.join('https://github.com/gohugo.io/releases/download/v0.62.0', TARBALL_FILENAME)
+
+DEB_FILENAME = 'hugo_0.62.0_Linux-64bit.deb'
+DEB_CHECKSUM="79e4594a891bf5b04997b175d549da6f3e3f913cd19c69fef7c289e50c63867c"
 
 # If absolute file path exists and checksum do not match #
 if [ $(sha256sum "$HOME/Downloads/$HUGO_DOWNLOAD_DEB") != "$HUGO_DOWNLOAD_CHECKSUM $HOME/Downloads/$HUGO_DOWNLOAD_DEB" ];
