7e0448050939a97b40f1cbe8fe7c6d0c1364a6de
Ying Wang <yingw787.personal@gmail.com>
2019-12-26 16:54:14 -0500

    Update configure_bash.sh

--- ubuntu/configure_bash.sh
+++ ubuntu/configure_bash.sh
@@ -4,11 +4,15 @@
 #
 # Bash Version: UNVERSIONED
 
+LOG_PREFIX="[https://dotfiles.yingw787.com]"
+
 # Make a copy of the existing .bashrc before overwriting it. #
 cp $HOME/.bashrc $HOME/.bashrc.copy
 
 # Overwrite .bashrc #
 cp $(pwd)/config/.bashrc $HOME/.bashrc
 
-# Source .bashrc for current shell to align shell contexts. #
-source $(pwd)/config/.bashrc
+# Cannot automatically source script because dependencies on this script would
+# need to be sourced too. Otherwise, 'source' only exists within the child
+# process, which would then be destroyed before leaving parent context.
+echo "$LOG_PREFIX NOTE: Current bash context is now outdated. Refresh shell by running 'source ~/.bashrc'."
