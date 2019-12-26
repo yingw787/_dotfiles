da682941f7932efcb95ea862f582d90f0674d4fa
Ying Wang <yingw787.personal@gmail.com>
2019-12-26 16:33:24 -0500

    Update setup-ubuntu.sh.

--- ubuntu/setup-ubuntu.sh
+++ ubuntu/setup-ubuntu.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
-
+#
 # Initial setup script.
-
+#
 # Assumptions:
 #
 # Using operating system Ubuntu 19.10.0 LTS (Eoan Ermine)
 # 'git' installed from ppa:git-core/ppa
-
+#
 # Optional arguments:
 #
 # [--no-gui]: Install non-GUI tools only (TODO)
-
+#
+# NOTE: All scripts (*.sh, *.py) should only contain system dependencies. *.sh
+# (shell scripts) should use system default shell 'bash' (/bin/bash), and *.py
+# (Python scripts) should use system default Python 3.7.x (/usr/bin/python3.7).
+#
+# Using a mixture of shell and Python scripts. Python where possible as a
+# general-purpose language has more robust / defined behavior than what 'bash'
+# can provide, and 'bash' where only 'bash' compatibility exists.
+#
+# NOTE: All scripts should be run as 'sudo -Hu $(whoami) ./script' in order to
+# leave environment variables dependent on user context (e.g. $HOME) untouched.
 
 WHOAMI=$(whoami)
 
