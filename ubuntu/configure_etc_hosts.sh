144d49a6954d124e49da21138815d38915d68e73
Ying Wang <yingw787.personal@gmail.com>
2019-12-26 16:55:44 -0500

    Update configure_etc_hosts.sh.

--- ubuntu/configure_etc_hosts.sh
+++ ubuntu/configure_etc_hosts.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #
 # Configures /etc/hosts file:
-# - Copies file '/etc/hosts' to '/etc/hosts.original'
+# - Copies file '/etc/hosts' to '/etc/hosts.copy'
 # - Copies file 'config/hosts' to '/etc/hosts'
 
-sudo cp /etc/hosts /etc/hosts.original
+LOG_PREFIX = "[https://dotfiles.yingw787.com]"
+
+sudo cp /etc/hosts /etc/hosts.copy
 sudo cp $(pwd)/config/hosts /etc/hosts
+
+echo "$LOG_PREFIX Updated file '/etc/hosts'."
