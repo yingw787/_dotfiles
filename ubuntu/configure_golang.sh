9b70f3cc5aed8bff7c506b7008c5c91700c4c3ce
Ying Wang <yingw787.personal@gmail.com>
2019-12-25 23:40:56 -0500

    Update configure_golang.sh.

--- ubuntu/configure_golang.sh
+++ ubuntu/configure_golang.sh
@@ -7,8 +7,16 @@
 GOLANG_DOWNLOAD_FILENAME="go1.13.5.linux-amd64.tar.gz"
 GOLANG_DOWNLOAD_LINK="https://dl.google.com/go/$GOLANG_DOWNLOAD_FILENAME"
 
+# Download go tarball #
 wget \
     $GOLANG_DOWNLOAD_LINK \
     --directory-prefix "$HOME/Downloads"
 
+# Untar the tarball and place within directory '/usr/local' #
 tar -C /usr/local -xzf "$HOME/Downloads/$GOLANG_DOWNLOAD_FILENAME"
+
+# Set up environment variable '$PATH' #
+#
+# Done as part of copying over file 'dotfiles/ubuntu/config/.bashrc' to location
+# '$HOME/.bashrc'. See files './configure_bash.sh' and './setup-ubuntu.sh' for
+# more details.
