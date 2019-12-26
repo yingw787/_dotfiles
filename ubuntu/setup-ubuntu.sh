181d27b2261adb89374f7014e30c6f4930f6e001
Ying Wang <yingw787.personal@gmail.com>
2019-12-26 16:28:38 -0500

    Update setup-ubuntu.sh.

--- ubuntu/setup-ubuntu.sh
+++ ubuntu/setup-ubuntu.sh
@@ -4,12 +4,15 @@
 
 # Assumptions:
 #
-# Operating System: Ubuntu 19.10.0 LTS (Eoan Ermine)
-
-# Initial startup before executing startup scripts:
-#
-# - Ran script $(pwd)/../setup.sh
+# Using operating system Ubuntu 19.10.0 LTS (Eoan Ermine)
+# 'git' installed from ppa:git-core/ppa
 
 # Optional arguments:
 #
 # [--no-gui]: Install non-GUI tools only (TODO)
+
+
+WHOAMI=$(whoami)
+
+sudo -Hu $WHOAMI /usr/bin/python3.7 $(pwd)/configure_golang.py
+sudo -Hu $WHOAMI /usr/bin/python3.7 $(pwd)/configure_hugo.py
