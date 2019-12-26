55335c34f761812fb7f0207fc3e9d87e65267e01
Ying Wang <yingw787.personal@gmail.com>
2019-12-26 15:59:34 -0500

    Update configure_golang.sh thinking about migrating from shell to system python3.7 since it is available.

--- ubuntu/configure_golang.sh
+++ ubuntu/configure_golang.sh
@@ -3,17 +3,37 @@
 # Configures the user 'golang' setup.
 #
 # Version: v1.13.5
+#
+# Usage:
+#
+# - 'sudo -Hu $(whoami) ./configure_golang.sh'
 
-GOLANG_DOWNLOAD_FILENAME="go1.13.5.linux-amd64.tar.gz"
-GOLANG_DOWNLOAD_LINK="https://dl.google.com/go/$GOLANG_DOWNLOAD_FILENAME"
+LOG_PREFIX="[https://dotfiles.yingw787.com]"
 
-# Download go tarball #
-wget \
-    $GOLANG_DOWNLOAD_LINK \
-    --directory-prefix "$HOME/Downloads"
+DOWNLOADS="$HOME/Downloads"
+
+TARBALL_FILENAME="go1.13.5.linux-amd64.tar.gz"
+TARBALL_URI="https://dl.google.com/go/$TARBALL_FILENAME"
+
+TARBALL_CHECKSUM="512103d7ad296467814a6e3f635631bd35574cab3369a97a323c9a585ccaa569"
+
+echo "$LOG_PREFIX Download $TARBALL_URI to $DOWNLOADS."
+
+file_exists=$(test -f $DOWNLOADS/$TARBALL_FILENAME)
+
+if [ -f "$DOWNLOADS/$TARBALL_FILENAME" && $(sha256sum "$DOWNLOADS/$TARBALL_FILENAME") == "$TARBALL_CHECKSUM $DOWNLOADS/$TARBALL_FILENAME"];
+then
+    echo "$LOG_PREFIX Tarball downloaded and checksum validation passes. Skipping download."
+else
+    # Download go tarball #
+    wget \
+        $TARBALL_URI \
+        --directory-prefix "$HOME/Downloads"
+fi
 
 # Untar the tarball and place within directory '/usr/local' #
-sudo tar -C /usr/local -xzf "$HOME/Downloads/$GOLANG_DOWNLOAD_FILENAME"
+echo "$LOG_PREFIX Install tarball."
+sudo tar -C /usr/local -xzf "$HOME/Downloads/$TARBALL_FILENAME"
 
 # Set up environment variable '$PATH' #
 #
