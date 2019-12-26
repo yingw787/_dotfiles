158d6b63a009fde8125764010af7e9785b5bb623
Ying Wang <yingw787.personal@gmail.com>
2019-12-25 22:04:14 -0500

    Update configure_gnome.sh [BROKEN] Adding GNOME Dash-to-Dock extension via package.

--- ubuntu/configure_gnome.sh
+++ ubuntu/configure_gnome.sh
@@ -21,3 +21,23 @@ apt-get install adapta-gtk-theme
 # - "Themes/Icons" : "Papirus"
 # - "Themes/Shell" : "N/A"
 # - "Themes/Sound" : "Yaru"
+
+# Install GNOME Dash-to-Dock extension (dependency: GNOME Shell v3.34.1) #
+$GNOME_ZIPFILE='12397.shell-extension.zip'
+wget \
+    "https://extensions.gnome.org/review/download/$GNOME_ZIPFILE" \
+    --directory-prefix "$HOME/Downloads"
+
+$DASH_TO_DOCK_DIRNAME='dash-to-dock@micxgx.gmail.com'
+$DASH_TO_DOCK_TMP_ABSPATH="$HOME/Downloads/$DASH_TO_DOCK_DIRNAME"
+
+mkdir $DASH_TO_DOCK_TMP_ABSPATH
+unzip "$HOME/Downloads/$GNOME_ZIPFILE" -d $DASH_TO_DOCK_TMP_ABSPATH
+
+$GNOME_SHELL_EXTENSIONS="$HOME/.local/share/gnome-shell/extensions"
+if ! [ -d $GNOME_SHELL_EXTENSIONS ];
+then
+    mkdir -p $GNOME_SHELL_EXTENSIONS
+fi
+
+mv $DASH_TO_DOCK_TMP_ABSPATH "$GNOME_SHELL_EXTENSIONS/$DASH_TO_DOCK_DIRNAME"
