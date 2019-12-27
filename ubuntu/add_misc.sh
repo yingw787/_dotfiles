b1e498e1a1835d15708238fb7ab40eb39a9b94c4
Ying Wang <yingw787.personal@gmail.com>
2019-12-27 15:43:11 -0500

    Update add_misc.sh.

--- ubuntu/add_misc.sh
+++ ubuntu/add_misc.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 #
 # Add miscellaneous programs that do not require special configuration.
+#
+# Usage:
+# - 'sudo -Hu $(whoami) /bin/bash add_misc.sh'
 
-# Install `tree` build `tree/eoan 1.8.0-1 amd64` #
-apt-get install tree=1.8.0-1
+LOG_PREFIX="[https://dotfiles.yingw787.com]"
 
-# Install `xclip` build `xclip/eoan,nwo 0.13-1 amd64` #
-apt-get install xclip=0.13-1
+# Install 'tree' build 'tree/eoan 1.8.0-1 amd64' #
+echo "$LOG_PREFIX install 'tree' build 'tree/eoan 1.8.0-1 amd64'."
+sudo apt-get install -y tree=1.8.0-1
+
+# Install 'xclip' build 'xclip/eoan,nwo 0.13-1 amd64' #
+echo "$LOG_PREFIX install 'xclip' build 'xclip/eoan,nwo 0.13-1 amd64'."
+sudo apt-get install -y xclip=0.13-1
+
+# Install 'ncdu' build 'ncdu/eoan 1.14.1-1 amd64' #
+echo "$LOG_PREFIX install 'ncdu' build 'ncdu/eoan 1.14.1-1 amd64'."
+sudo apt-get install -y ncdu=1.14.1-1
