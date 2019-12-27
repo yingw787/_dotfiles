a858ecd7bc062ede73aef07fdf20998f9fdb0151
Ying Wang <yingw787.personal@gmail.com>
2019-12-27 15:00:25 -0500

    Update download_git_projects.sh got to working state.

--- projects/download_git_projects.sh
+++ projects/download_git_projects.sh
@@ -1,28 +1,52 @@
 #!/bin/bash
 #
-# Download currently relevant 'git' projects.
+# Download registered 'git' projects.
 #
 # Assumptions:
 #
 # - 'git' is available and fully configured.
-# - SSH keys are registered at remote.
+# - SSH keys are registered at remote (GitHub / GitLab / etc.)
+#
+# Usage:
+# - '/bin/bash $HOME/dotfiles/projects/download_git_projects.sh'
 
-# Make directory and pushd into it #
-mkdir $HOME/src
+LOG_PREFIX="[https://dotfiles.yingw787.com]"
+
+echo "$LOG_PREFIX creating directory if not exists '$HOME/src'."
+mkdir -p $HOME/src
 pushd $HOME/src
     # Bytes by Ying #
-    git clone git@gitlab.com:yingw787/bytes.yingw787.com.git
+    BYTES_BY_YING="git@gitlab.com:yingw787/bytes.yingw787.com.git"
+    echo "$LOG_PREFIX cloning '$BYTES_BY_YING'."
+    git clone $BYTES_BY_YING
 
     # Personal blog #
-    git clone git@gitlab.com:yingw787/blog.yingw787.com.git
+    BLOG_BY_YING="git@gitlab.com:yingw787/blog.yingw787.com.git"
+    echo "$LOG_PREFIX cloning '$BLOG_BY_YING'."
+    git clone $BLOG_BY_YING
 
     # Resume #
-    git clone git@github.com:yingw787/resume.yingw787.com.git
+    RESUME="git@github.com:yingw787/resume.yingw787.com.git"
+    echo "$LOG_PREFIX cloning '$RESUME'."
+    git clone $RESUME
 
     # Projects #
-    git clone git@github.com:yingw787/projects.yingw787.com.git
+    PROJECTS="git@gitlab.com:yingw787/projects.yingw787.com.git"
+    echo "$LOG_PREFIX cloning '$PROJECTS'."
+    git clone $PROJECTS
 
     # Homepage #
-    git clone git@github.com:yingw787/yingw787.com.git
-# Exit pushd context #    
+    HOMEPAGE="git@github.com:yingw787/yingw787.com.git"
+    echo "$LOG_PREFIX cloning '$HOMEPAGE'."
+    git clone $HOMEPAGE
+
+    # Dotfiles website #
+    DOTFILES_WEBSITE="git@github.com:yingw787/dotfiles.yingw787.com.git"
+    echo "$LOG_PREFIX cloning '$DOTFILES_WEBSITE'."
+    git clone $DOTFILES_WEBSITE
+
+    # TinyDevCRM "Coming Soon" webpage #
+    TINYDEVCRM_COMINGSOON="git@github.com:yingw787/tinydevcrm-comingsoon.git"
+    echo "$LOG_PREFIX cloning '$TINYDEVCRM_COMINGSOON'."
+    git clone $TINYDEVCRM_COMINGSOON
 popd
