c5911927dfb6384165fd457e714a1dd4651b49ff
Ying Wang <yingw787.personal@gmail.com>
2019-12-27 17:05:09 -0500

    Rename configure_python.sh to configure_python.py, and begin to add scripts.

---
+++ ubuntu/configure_python.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3.7
+#
+# Configures the user 'python3' setup.
+#
+# This installs Anaconda release '2019.10' as primary user Python.
+#
+# Usage:
+# - 'sudo -Hu $(whoami) /usr/bin/python3.7 configure_python.py'
+
+import os
+
+
+LOG_PREFIX = '[https://dotfiles.yingw787.com]'
+
+HOME = os.environ['HOME']
+DOWNLOADS = os.path.join(HOME, 'Downloads')
+
+CONDA_SCRIPT_NAME = 'Anaconda3-2019.10-Linux-x86_64.sh'
+CONDA_SCRIPT_URI = os.path.join(
+    'https://repo.anaconda.com/archive/',
+    CONDA_SCRIPT_NAME
+)
