5ba05e2e3023f765395c78e77c0e78ebf29388e7
Ying Wang <yingw787.personal@gmail.com>
2019-12-27 15:56:45 -0500

    Update configure_vim.sh.

--- ubuntu/configure_vim.sh
+++ ubuntu/configure_vim.sh
@@ -6,3 +6,10 @@
 # - 'sudo -Hu $(whoami) /bin/bash configure_vim.sh'
 
 LOG_PREFIX="[https://dotfiles.yingw787.com]"
+
+echo "$LOG_PREFIX install 'vim' build 'vim/eoan 2:8.1.0875-5ubuntu2 amd64'."
+sudo apt-get install -y \
+    vim=2:8.1.0875-5ubuntu2 \
+    vim-scripts=20180807ubuntu1 \
+    vim-doc=2:8.1.0875-5ubuntu2 \
+    exuberant-ctags=1:5.9~svn20110310-12
