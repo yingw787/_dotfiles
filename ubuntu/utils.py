77f7f289cda50fd3e2212c636ad9569b19fd7882
Ying Wang <yingw787.personal@gmail.com>
2019-12-27 16:56:49 -0500

    Update utils.py add one method.

--- ubuntu/utils.py
+++ ubuntu/utils.py
@@ -2,6 +2,26 @@
 #
 # Utility methods applicable to setup.
 
-def download_file():
+import subprocess
+
+
+def generate_sha256sum_of_file(filename_abspath):
+    """Generates a SHA-256 checksum of a file.
+
+    Args:
+        filename_abspath (str)
+
+    Returns:
+        str: SHA-256 checksum.
     """
-    """
+    bash_command = ['sha256sum', filename_abspath]
+    process = subprocess.Popen(
+        bash_command,
+        stdout=subprocess.PIPE
+    )
+    output = process.stdout.read()
+    output = output.decode('utf-8')
+
+    checksum = output.split(' ')[0]
+
+    return checksum
