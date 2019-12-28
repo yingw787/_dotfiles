afe617e300df567ab58a99ff689010bba760795d
Ying Wang <yingw787.personal@gmail.com>
2019-12-27 23:33:42 -0500

    Update configure_ssh.sh.

--- ubuntu/configure_ssh.sh
+++ ubuntu/configure_ssh.sh
@@ -9,11 +9,15 @@
 #
 # --override (optional): Override the existing SSH public/private keypair.
 
+LOG_PREFIX="[https://dotfiles.yingw787.com]"
+
 # Generate SSH public/private keypair. #
-ssh-keygen \
-    -t rsa \ # '-t': Type of key to be generated.
-    -b 4096 \ # '-b': Number of bits in the key to create.
-    -C $EMAIL \ # '-C': Provide a new comment.
-    -f "$_HOME/.ssh/id_rsa" # '-f': Filename of the key file.
-    -q \ # '-q': Silence ssh-keygen.
-    -N "" # No passphrase by default.
+#
+# '-t': Type of key to be generated.
+# '-b': Number of bits in the key to create.
+# '-C': Provide a new comment.
+# '-f': Filename of the key file.
+# '-q': Silence ssh-keygen.
+# '-N': No passphrase by default.
+echo "$LOG_PREFIX Run cmd 'ssh-keygen -t rsa -b 4096 -f \"$_HOME/.ssh/id_rsa\" -q -N \"\"'."
+ssh-keygen -t rsa -b 4096 -f "$HOME/.ssh/id_rsa" -q -N ""
